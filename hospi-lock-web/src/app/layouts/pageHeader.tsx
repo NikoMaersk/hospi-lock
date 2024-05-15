224208e59b998b70d9647a0a5783869afb390983
NikoMaersk <maerskp94@gmail.com>
2024-05-15 11:43:32 +0200

    Added user icon to PageHeader

--- hospi-lock-web/src/app/layouts/pageHeader.tsx
+++ hospi-lock-web/src/app/layouts/pageHeader.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { Bell, HelpCircle, Menu, Search, ArrowLeft } from "lucide-react";
-import { Button } from "../components/button";
+import { Button, buttonStyles } from "../components/button";
 import React, { useRef, useState } from 'react';
+import { twMerge } from "tailwind-merge";
 
 export default function PageHeader() {
     const inputRef = useRef<HTMLInputElement>(null);
@@ -14,11 +15,13 @@ export default function PageHeader() {
                 <Button variant="ghost">
                     <Menu className="stroke-gray-600" />
                 </Button>
+                <a className={twMerge(buttonStyles({ variant: "ghost" }),)}>
+                    <img />
+                </a>
             </nav>
             <form
                 className={`gap-4 my-3 flex-grow justify-center ${showFullWidthSearch ? "flex" : "hidden md:flex"
-                    }`}
-            >
+                    }`}>
                 {showFullWidthSearch && (
                     <Button
                         onClick={() => setShowFullWidthSearch(false)}
@@ -28,27 +31,29 @@ export default function PageHeader() {
                         <ArrowLeft />
                     </Button>
                 )}
-                <div className="flex flex-grow max-w-[600px]">
+                <div className="flex flex-grow max-w-[600px] border border-secondary-border rounded-md">
+                    <Button variant="none" size="default" className="px-4 rounded-l-md flex-shrink-0 bg-shadow-secondary">
+                        <Search className="stroke-gray-500"/>
+                    </Button>
                     <input
                         type="search"
                         placeholder="Search"
-                        className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+                        className=" rounded-r-md shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none text-gray-800"
                     />
-                    <Button size="default" className="px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
-                        <Search />
-                    </Button>
                 </div>
             </form>
-            <div  className={`flex-shrink-0 md:gap-2 ${
-          showFullWidthSearch ? "hidden" : "flex"
-        }`}>
-                
+            <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"
+                }`}>
                 <Button variant="ghost">
                     <HelpCircle className="stroke-gray-600" />
                 </Button>
                 <Button variant="ghost">
                     <Bell className="stroke-gray-600" />
                 </Button>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" className={twMerge(buttonStyles({ variant: "ghost" }), ``)}>
+                    <img src="https://randomuser.me/api/portraits/med/women/10.jpg"
+                        className="rounded-full" />
+                </a>
             </div>
         </header>
     );
