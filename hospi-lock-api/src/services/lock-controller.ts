ae22d1d20766624f189c40a4e72ff90cc99bbc22
NikoMaersk <maerskp94@gmail.com>
2024-05-22 11:30:07 +0200

    upgraded to node: 18

--- hospi-lock-api/src/services/lock-controller.ts
+++ hospi-lock-api/src/services/lock-controller.ts
@@ -5,15 +5,15 @@ export default class LockController {
     private static PORT: string = process.env.PORT || "5000";
 
 
-    static async unlockAsync(): Promise<{ success: boolean, message: string }> {
+    static async unlockAsync(): Promise<{ success: boolean, message: string | unknown }> {
         return LockController.requestAsync('unlock');
     }
 
-    static async lockAsync(): Promise<{ success: boolean, message: string }> {
+    static async lockAsync(): Promise<{ success: boolean, message: string | unknown }> {
         return LockController.requestAsync('lock');
     }
 
-    private static async requestAsync(endpoint: string): Promise<{ success: boolean, message: string }> {
+    private static async requestAsync(endpoint: string): Promise<{ success: boolean, message: string | unknown }> {
 
         try {
             const response = await fetch(`http://${LockController.IP}:${LockController.PORT}/${endpoint}`, {
